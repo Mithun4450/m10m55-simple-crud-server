2d9885faa807b2778f3a609bc0c3a7ee7d7d93ae
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2023-10-14 23:09:56 +0600

    first commit

---
+++ ___steps.js
@@ -0,0 +1,27 @@
+/**
+ * --------------------------
+ * Mongodb Connection
+ * --------------------------
+ * 1. create account
+ * 2. create an user with password
+ * 3. whitelist IP address
+ * 4. database > connect > driver > Node > view full code
+ * 5. change the password in the uri
+ * 
+ * ----------------------------
+ * CREATE POST
+ * ----------------------------
+ * 1. app.post('/users', async(req, res) => {})
+ * 2. make the function async to use await inside it
+ * 3. Make sure you use the express.json() middleware
+ * 4. access data from the body: const user = req.body
+ * 5. const result await userCollection.insertOne(user)
+ * 6. res.send(result)
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *  */ 
